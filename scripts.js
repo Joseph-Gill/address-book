1199d51afedadad354dd6d8d7f31a878ccdeb010
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-10-24 18:16:49 +0200

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,133 @@
+// Mock Database for Address Book
+
+const database = {
+  addresses: [
+    {
+      id: 0,
+      name: "Joseph Gill",
+      address: "Förrlibuckstrasse 189, 8005 Zurich",
+      phoneNumber: "+41 78 731 22 76"
+    },
+    {
+      id: 1,
+      name: "Kimberly Hiller",
+      address: "Badenerstrasse 678, 8048 Zurich",
+      phoneNumber: "+41 78 732 7654"
+    },
+    {
+      id: 2,
+      name: "Linda Garbis",
+      address: "Josefstrasse 225, 8005 Zurich",
+      phoneNumber: "+41 78 731 99 45"
+    },
+    {
+      id: 3,
+      name: "Taso Garbis",
+      address: "Letzigraben 176, 8047 Zurich",
+      phoneNumber: "+41 78 732 88 13"
+    },
+    {
+      id: 4,
+      name: "Habib Kadiri",
+      address: "Bellerivestrasse 138, 8008 Zurich",
+      phoneNumber: "+41 78 731 1793"
+    },
+  ]
+};
+
+server = sinon.fakeServer.create();
+server.autoRespond = true;
+server.respondWith('Get', '/api/addresses',
+  [200, {'Content-Type': 'application/json'}, JSON.stringify(database.addresses)]
+);
+
+// Model for Single Address
+const Address = Backbone.Model.extend({
+  defaults: {
+    name: "",
+    address: "",
+    phoneNumber: ""
+  }
+});
+
+// Collection for All Addresses
+const Addresses = Backbone.Collection.extend({
+  model: Address,
+  url: "/api/addresses",
+});
+
+const addresses = new Addresses();
+
+// View for One Address
+const AddressView = Backbone.View.extend({
+  model: new Address(),
+  tagName: 'tr',
+  initialize: function () {
+    this.template = _.template($('.addresses-list-template').html());
+  },
+  events: {
+    'click .delete-address': 'delete'
+  },
+  delete: function () {
+    this.model.destroy();
+    $('.address-input-row').show();
+  },
+  render: function () {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
+})
+
+// View for All Addresses
+const AddressesView = Backbone.View.extend({
+  model: addresses,
+  el: $('.addresses-list'),
+  initialize: function () {
+    this.model.on('add', this.render, this);
+    this.model.on('remove', this.render, this);
+  },
+  render: function () {
+    let self = this;
+    this.$el.html('');
+    _.each(this.model.toArray(), function (address) {
+      self.$el.append((new AddressView({model: address})).render().$el);
+    })
+    return this;
+  }
+});
+
+const addressesView = new AddressesView();
+
+// Fetches Addresses stored in the mock server
+addresses.fetch()
+
+$(document).ready(function () {
+  $('.add-address').on('click', function () {
+    // Triggers error message if Phone Input Field does not pass Regex Test
+    if ($('.phoneNumber-input').is(':invalid')) {
+      // Toggles Error Display to visible
+      $('.phoneNumberError').css('display', 'inline');
+    } else {
+      // Resets Error Display to invisible
+      $('.phoneNumberError').css('display', 'none');
+      // Creates new Model of Address
+      const address = new Address({
+        name: $('.name-input').val(),
+        address: $('.address-input').val(),
+        phoneNumber: $('.phoneNumber-input').val()
+      });
+      // Resets Input fields to be blank
+      $('.name-input').val('');
+      $('.address-input').val('');
+      $('.phoneNumber-input').val('');
+      // Adds New Address to current Collection
+      addresses.add(address)
+      // Checks number of Addresses and disables the field if at 10 or more
+      if (addresses.length >= 10) {
+        $('.address-input-row').hide();
+      } else {
+        $('.address-input-row').show();
+      }
+    }
+  });
+})
